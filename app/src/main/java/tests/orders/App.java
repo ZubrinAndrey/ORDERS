ce06ad78340ca918c90b9feefd6d144330935c0f
ZubrinAndrey <zubrin@inbox.ru>
2017-04-23 23:53:14 +0700

    view holder

--- app/src/main/java/tests/orders/App.java
+++ app/src/main/java/tests/orders/App.java
@@ -3,14 +3,11 @@ package tests.orders;
 import android.app.Application;
 import android.widget.Toast;
 
-
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Toast.makeText(this, "NEW APP CREATED", Toast.LENGTH_SHORT).show();
+    //    Toast.makeText(this, "NEW APP CREATED", Toast.LENGTH_SHORT).show();
     }
-
-
 }
--- app/src/main/java/tests/orders/orderlist/CustomAdapterBase.java
+++ app/src/main/java/tests/orders/orderlist/CustomAdapterBase.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
 import java.util.ArrayList;
+
 import tests.orders.R;
 
 public class CustomAdapterBase extends BaseAdapter {
@@ -14,9 +16,15 @@ public class CustomAdapterBase extends BaseAdapter {
     private ArrayList<String> orders;
     private Context context;
 
-    public CustomAdapterBase(Context context, ArrayList<String> orders){
+    public CustomAdapterBase(Context context, ArrayList<String> orders) {
         this.orders = orders;
         this.context = context;
+    }
+
+    static class ViewHolder {
+        TextView tvPrice;
+        TextView tvSize;
+        TextView tvDescr;
     }
 
     @Override
@@ -38,21 +46,30 @@ public class CustomAdapterBase extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         View view = convertView;
+        ViewHolder viewHolder;
+
         if (view == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.tvDescr = (TextView) view.findViewById(R.id.tvDescr);
+            viewHolder.tvPrice = (TextView) view.findViewById(R.id.tvPrice);
+            viewHolder.tvSize = (TextView) view.findViewById(R.id.tvSize);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
         }
 
-        String p = getProduct(position);
+        String p = getOrder(position);
 
-        ((TextView) view.findViewById(R.id.tvDescr)).setText(p);
-        ((TextView) view.findViewById(R.id.tvPrice)).setText("Стоимость 4500$");
-        ((TextView) view.findViewById(R.id.tvSize)).setText("Обьем 3м куб");
+        viewHolder.tvDescr.setText(p);
+        viewHolder.tvPrice.setText("Стоимость 4500$");
+        viewHolder.tvSize.setText("Обьем 3м куб");
 
         return view;
     }
 
-    String getProduct(int position) {
+    String getOrder(int position) {
         return ((String) getItem(position));
     }
 }
