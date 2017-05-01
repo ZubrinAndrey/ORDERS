50ca802cfb0738fad1a5c983499ec88d109822b7
ZubrinAndrey <zubrin@inbox.ru>
2017-05-01 19:38:12 +0700

    плюс логаут

--- app/src/main/java/tests/orders/orderdetails/OrderDetailsActivity.java
+++ app/src/main/java/tests/orders/orderdetails/OrderDetailsActivity.java
@@ -2,17 +2,29 @@ package tests.orders.orderdetails;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import tests.orders.R;
+import tests.orders.authentication.AuthActivity;
+import tests.orders.orderlist.OrderListActivity;
 
-public class OrderDetailsActivity extends AppCompatActivity {
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+public class OrderDetailsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FragmentManager mFragmentManager;
     private FragmentTransaction mFragmentTransaction;
@@ -35,19 +47,50 @@ public class OrderDetailsActivity extends AppCompatActivity {
         mFragmentTransaction.add(R.id.container3, orderDetailsFragment);
         mFragmentTransaction.commit();
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout3);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view3);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        TextView textUserName = (TextView) header.findViewById(R.id.textUserName);
+        textUserName.setText(getIntent().getStringExtra(OrderListActivity.KEY_LOGIN));   // не работает
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)) {
+        if (mToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_account) {
+
+            Toast.makeText(this, "MY ACCOUNT", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_settings) {
+            Toast.makeText(this, "SETTINGS", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.nav_logout) {
+            Intent intent = new Intent(this, AuthActivity.class);
+            intent.addFlags(FLAG_ACTIVITY_CLEAR_TASK);
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            Toast.makeText(this, "LOGOUT", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout3);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
 
--- app/src/main/java/tests/orders/orderlist/OrderListActivity.java
+++ app/src/main/java/tests/orders/orderlist/OrderListActivity.java
@@ -7,14 +7,25 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import tests.orders.R;
+import tests.orders.authentication.AuthActivity;
 
-public class OrderListActivity extends AppCompatActivity {
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static tests.orders.R.id.nav_view2;
+
+public class OrderListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     public static final String KEY_LOGIN = "login";
 
@@ -43,11 +54,18 @@ public class OrderListActivity extends AppCompatActivity {
         mFragmentTransaction.add(R.id.container2, orderListFragment);
         mFragmentTransaction.commit();
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout2);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(nav_view2);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        TextView textUserName = (TextView)header.findViewById(R.id.textUserName);
+        textUserName.setText(getIntent().getStringExtra(KEY_LOGIN));
     }
 
     @Override
@@ -74,5 +92,27 @@ public class OrderListActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_account) {
+            Toast.makeText(this, "MY ACCOUNT", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_settings) {
+            Toast.makeText(this, "SETTINGS", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.nav_logout) {
+            Intent intent = new Intent(this, AuthActivity.class);
+            intent.addFlags(FLAG_ACTIVITY_CLEAR_TASK);
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            Toast.makeText(this, "LOGOUT", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout2);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
